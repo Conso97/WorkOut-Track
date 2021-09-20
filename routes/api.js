aef4346fbef29d7308a0b5c13d6ee764c3f84e65
Consolata Njeri <conso4u@gmail.com>
2021-09-19 19:16:46 -0700

    serving new exercise page

---
+++ routes/api.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+
+
+router.get('/api', (req, res) => {
+
+});
+
+router.get('/api/workout', (req, res) => {
+
+});
+
+router.get('/api/fetch', (req, res) => {
+
+});
+
+module.exports = router;
