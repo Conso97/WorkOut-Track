89b3029df108562b289febf3b52f2bb79cfb35a3
Consolata Njeri <conso4u@gmail.com>
2021-10-18 15:11:08 -0700

    deploying

--- server.js
+++ server.js
@@ -14,9 +14,11 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-mongoose.connect('mongodb://localhost/workout', {
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
     useNewUrlParser: true,
     useFindAndModify: false,
+    useCreateIndex: true,
     useUnifiedTopology: true, 
 });
 
